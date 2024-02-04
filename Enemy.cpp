7ab8445ef1b841430e1ac0820e3776be5d01baf6
隼 石井 <k023g0022@g.neec.ac.jp>
2024-02-04 23:05:37 +0900

    a

--- Enemy.cpp
+++ Enemy.cpp
@@ -49,3 +49,30 @@ void Enemy::Hit(Vector2 bullet1) {
 		}
 	}	
 }
+
+
+
+Enemy::Enemy(int posX, int posY, int speedX, int speedY)
+{
+	posX_ = posX;
+	posY_ = posY;
+	speedX_ = speedX;
+	speedY_ = speedY;
+	radius_ = 8;
+}
+
+void Enemy::Update() {
+	posX_ += speedX_;
+	posY_ += speedY_;
+
+	if (posX_ - radius_ <= 0 || posX_ + radius_ >= 1280) {
+		speedX_ *= -1;
+	}
+	if (posY_ - radius_ <= 0 || posY_ + radius_ >= 720) {
+		speedY_ *= -1;
+	}
+}
+
+void Enemy::Draw12() {
+	Novice::DrawEllipse(posX_, posY_, radius_, radius_, 0.0f, RED, kFillModeSolid);
+}
--- Enemy.h
+++ Enemy.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "Vector2.h"
 #include<Novice.h>
 #include"Player.h"
@@ -25,5 +25,53 @@ public:
 	void Hit(Vector2 bullet);
 
 	
+
+
+
+
+
+
+
+
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="posX">X座標</param>
+	/// <param name="posY">Y座標</param>
+	/// <param name="speedX">移動速度X</param>
+	/// <param name="speedY">移動速度Y</param>
+	Enemy(int posX, int posY, int speedX, int speedY);
+
+
+	int GetPosX() { return posX_; };
+	int GetPosY() { return posY_; };
+	int GetspeedX() { return speedX_; };
+	int GetspeedY() { return speedY_; };
+	int Getradius() { return radius_; };
+
+	void GetspeedX(int speed1) { this->speedX_ = speed1; };
+	void GetspeedY(int speed) { this->speedY_ = speed; };
+
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw12();
+
+
+private:	// メンバ変数
+	int posX_;
+	int posY_;
+	int speedX_;
+	int speedY_;
+	int radius_;
 };
+
+
 
